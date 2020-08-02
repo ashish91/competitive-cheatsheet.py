127d23daeb0cc62cfcbaaad248865f0d2c924047
Ashish Gaur <ashishgaurp91@gmail.com>
2020-08-02 14:25:54 +0530

    Array and dictionary helper methods

--- python-essentials/array.py
+++ python-essentials/array.py
@@ -2,13 +2,21 @@
 length = len(arr)
 
 # Looping
+# By Element
 for element in arr:
 	print(element)
+# By Index
 for index in range(arr):
 	print(arr[index])
+# By Index and Element
 for index, element in enumerate(arr):
 	print('Element: ' + element)
 	print('Index: ' + index)
+# Reverse
+for element in reversed(arr):
+	print('Element: ' + element)
+for index in range(len(arr)-1, -1, -1):
+	print('Element: ' + element)
 
 # Initialise 1D array
 arr = [1]*3 # [1, 1, 1]
@@ -17,3 +25,7 @@ arr = [ 1 for i in range(3) ]
 # Initialise 2D array
 matrix = [[0 for i in range(cols)] for j in range(rows)]
 matrix = [[0]*cols]*rows
+
+# Sorting an array
+arr.sort()
+arr.sort(reverse=True)
---
+++ python-essentials/dict.py
@@ -0,0 +1,6 @@
+# Initialization
+dictionary = dict()
+
+# Existence of Key
+if key in dictionary.keys():
+	print(key + 'is present')
