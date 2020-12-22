d1645c66c1e31eae9a79ff8d76d8e4210a5aa43e
Ashish Gaur <ashishgaurp91@gmail.com>
2020-12-22 20:58:17 +0530

    Insert/Remove arrays

--- python-essentials/array.py
+++ python-essentials/array.py
@@ -1,5 +1,3 @@
-## Initialization
-#
 # Initialise 1D array
 arr = [1]*3 # [1, 1, 1]
 arr = [ 1 for i in range(3) ]
@@ -8,19 +6,17 @@ arr = [ 1 for i in range(3) ]
 matrix = [[0 for c in range(cols)] for r in range(rows)]
 matrix = [[0]*cols]*rows
 
-## Looping
-#
-# By Element
+# Loop By Element
 for element in arr:
 	print(element)
-# By Index
+# Loop By Index
 for index in range(arr):
 	print(arr[index])
-# By Index and Element
+# Loop By Index and Element
 for index, element in enumerate(arr):
 	print('Element: ' + element)
 	print('Index: ' + index)
-# Reverse
+# Loop in Reverse
 for element in reversed(arr):
 	print('Element: ' + element)
 for index in range(len(arr)-1, -1, -1):
@@ -29,3 +25,23 @@ for index in range(len(arr)-1, -1, -1):
 # Sorting an array
 arr.sort()
 arr.sort(reverse=True)
+
+# Reverse
+arr = arr[::-1]
+
+# Subarray
+arr = [1,2,3,4]
+a[0:1]
+[1]
+a[2:4]
+[3,4]
+
+# Add element
+arr.append(24)
+# Add element at ith position
+arr.insert(i,24)
+
+# Remove element from back
+arr.pop()
+# Remove element at ith position
+arr.pop(i)
