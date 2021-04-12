63af81cc01514e32aa558ccfbfe6177ec8c9eefd
Ashish Gaur <ashishgaurp91@gmail.com>
2021-04-12 12:19:13 +0530

    Add defaultdict

--- python-essentials/dict.py
+++ python-essentials/dict.py
@@ -1,6 +1,10 @@
 # Initialization
 dictionary = dict()
 
+# counter dict with 0 initialized
+from collections import defaultdict
+counter_dict = defaultdict(int)
+
 # Existence of Key
 if key in dictionary.keys():
   print(key + 'is present')
