f717bab010c446f97e953f749096aba749c31b7d
Ashish Gaur <ashishgaurp91@gmail.com>
2021-12-02 09:40:20 +0530

    Fix fast exponent

--- math/fast_exponent.py
+++ math/fast_exponent.py
@@ -1,9 +1,7 @@
 def fast_exponent(num,exp):
-
   if exp == 0:
     return 1
-
-  if num%2 == 0:
-    return fast_exponent(num*num, exp/2)
+  if exp%2 == 0:
+    return fast_exponent(num*num, exp//2)
   else:
-    return num*fast_exponent(num*num, (exp-1)/2)
+    return num*fast_exponent(num, exp-1)
